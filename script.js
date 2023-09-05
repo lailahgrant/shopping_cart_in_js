c70e5c6183001390e0bfd1f137021b245efc8864
Lailah Grant <lailahgrant19@gmail.com>
2023-09-05 23:09:16 +0300

    Create the array-object
    Created the array-object to store information about the sold items

---
+++ script.js
@@ -0,0 +1,38 @@
+const product = [
+    {
+        id: 0,
+        image: 'image/phone.png',
+        title: 'Samsung Galaxy S Fold Open',
+        price: '1,200,000',
+    },
+    {
+        id: 1,
+        image: 'image/phone-1.png',
+        title: 'Huaweii p40',
+        price: '900,000',
+    },
+    {
+        id: 2,
+        image: 'image/phone-2.png',
+        title: 'Xiaomi Redmi Note 9',
+        price: '1,400,000',
+    },
+    {
+        id: 3,
+        image: 'image/micro.png',
+        title: 'Combi Grill Microwave',
+        price: '500,000',
+    },
+    {
+        id: 4,
+        image: 'image/micro-1.png',
+        title: 'Daewoo Retro Microwave',
+        price: '450,000',
+    },
+    {
+        id: 5,
+        image: 'image/kettle.png',
+        title: 'Tea Kettle',
+        price: '600,000',
+    },
+]
